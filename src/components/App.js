3f4bb94500a2904ae0fa76f7a69262c70a58f348
jayasankar123 <jayasankarreddy0@gmail.com>
2020-11-06 01:33:43 +0530

    firstVersion

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">ProjectName 1</h1>
+        <p data-ns-test="project-description">ProjectDescription1</p>
+        <h1 data-ns-test="project-name">ProjectName 2</h1>
+        <p data-ns-test="project-description">ProjectDescription2</p>
+      </>
+    );
+  }
 }
-
 
 export default App;
